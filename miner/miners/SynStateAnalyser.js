f7c3e3795f46445704abb7343f6d7f1a21c76b7a
Marion Dübendorfer <marion.duebendorfer@switch.ch>
2022-01-04 09:31:19 +0100

    add aggregate method in SynStateAnalyser

--- miner/miners/SynStateAnalyser.js
+++ miner/miners/SynStateAnalyser.js
@@ -92,6 +92,15 @@ class SynStateAnalyser extends AbstractPCAPAnalyser {
   }
 
   static aggregateResults (resultA, resultB) {
+    for (var key in resultA) {
+      if (resultB.hasOwnProperty(key)) {
+        resultB[key] += resultA[key]
+      }
+      else {
+        resultB[key] = resultA[key]
+      }
+    }
+    return resultB
   }
 
   static getAnalysisName () {
